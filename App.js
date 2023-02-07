f25d3cddfd14dfac2e1a2e941bc419f9d2634b49
Linnxw <120721205+Linnxw@users.noreply.github.com>
2023-02-07 18:55:52 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import Todolist from './Component/TodoList.js'
+import './App.css'
+function App() {
+  return <Todolist/>
+}
+
+export default App;
---
+++ TodoList.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+const date=new Date().toLocaleTimeString()
+const [times,setTimes]=useState(date)
+let updateDate=()=>{
+  let date=new Date().toLocaleTimeString()
+  setTimes(date)
+}
+setInterval(()=>{
+  updateDate()
+},1000)
+  return (
+    <>
+    <div className="clock"><h4 clasName="time"><i>{times}</i></h4></div>
+ <div className="wraper-title">
+  <h2 className="title">my list for today</h2>
+  </div>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+          </>
+          ) 
+
+}
+
+export default TodoList
